dade17831cee43a0692588d9bf1ea6e7724b8bd4
Viktor.A <viktor.antonyshyn@gmail.com>
2023-09-07 12:17:02 +0200

    Added class Project

--- src/main/java/com/Portafolio/PortafolioApplication.java
+++ src/main/java/com/Portafolio/PortafolioApplication.java
@@ -2,6 +2,7 @@ package com.Portafolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -12,6 +13,13 @@ public class PortafolioApplication {
 		SpringApplication.run(PortafolioApplication.class, args);
 	}
 
-	public List<String> getJob
+	@GetMapping(path = "/")
+	public List<String> getJob(){
+		return List.of(
+				"qwwe",
+				"fdgsfd",
+				"dfgs"
+		);
+	}
 
 }
---
+++ src/main/java/com/Portafolio/domain/model/Project.java
@@ -0,0 +1,78 @@
+package com.Portafolio.domain.model;
+
+public class Project {
+    private Integer id;
+    private String name;
+    private String description;
+    private String picture;
+    private String link1;
+    private String link2;
+
+    public Project() {
+    }
+
+    public Project(Integer id, String name, String description, String picture, String link1, String link2) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.picture = picture;
+        this.link1 = link1;
+        this.link2 = link2;
+    }
+
+    public Project(String name, String description, String picture, String link1, String link2) {
+        this.name = name;
+        this.description = description;
+        this.picture = picture;
+        this.link1 = link1;
+        this.link2 = link2;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getLink1() {
+        return link1;
+    }
+
+    public void setLink1(String link1) {
+        this.link1 = link1;
+    }
+
+    public String getLink2() {
+        return link2;
+    }
+
+    public void setLink2(String link2) {
+        this.link2 = link2;
+    }
+}
