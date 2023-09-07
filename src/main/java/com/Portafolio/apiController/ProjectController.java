6b2311984949b2539f264037d7f6c05c9022ec56
Viktor.A <viktor.antonyshyn@gmail.com>
2023-09-07 18:26:26 +0200

    Added path = api/v1/projects

--- src/main/java/com/Portafolio/apiController/ProjectController.java
+++ src/main/java/com/Portafolio/apiController/ProjectController.java
@@ -2,13 +2,15 @@ package com.Portafolio.apiController;
 
 import com.Portafolio.domain.model.Project;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "api/v1/projects")
 public class ProjectController {
-    @GetMapping(path = "/")
+    @GetMapping
     public List<Project> getJob(){
         return List.of(
                 new Project(
