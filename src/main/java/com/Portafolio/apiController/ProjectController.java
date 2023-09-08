af36f605380efc079e3fadada54ee65d3c24f9ad
Viktor.A <viktor.antonyshyn@gmail.com>
2023-09-08 11:21:00 +0200

    Added @CrossOrigin

--- src/main/java/com/Portafolio/apiController/ProjectController.java
+++ src/main/java/com/Portafolio/apiController/ProjectController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:5174")
 @RestController
 @RequestMapping(path = "api/v1/projects")
 public class ProjectController {
