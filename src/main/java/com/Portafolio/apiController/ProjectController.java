9351c741b0f1d6b5e85a18b8e8267085c48d6118
Viktor.A <viktor.antonyshyn@gmail.com>
2023-09-12 14:43:44 +0200

    Modificar Controller y Service para obtener obtener el project por ID y devolverla en la respuesta ResponseEntity

--- src/main/java/com/Portafolio/apiController/ProjectController.java
+++ src/main/java/com/Portafolio/apiController/ProjectController.java
@@ -3,9 +3,11 @@ package com.Portafolio.apiController;
 import com.Portafolio.domain.model.Project;
 import com.Portafolio.domain.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 @CrossOrigin(origins = "http://localhost:5174")
 @RestController
 @RequestMapping(path = "api/v1/projects")
@@ -21,6 +23,17 @@ public class ProjectController {
     public List<Project> getJobController(){
         return projectService.getJob();
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Project> getProjectPorId(@PathVariable Integer id) {
+        // Lógica para obtener el project por ID y devolverla en la respuesta ResponseEntity
+        Project project = projectService.projectById(id);
+        if (project != null ) {
+            return ResponseEntity.ok(project);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @PostMapping
     public void create(@RequestBody Project project){
          projectService.newJob(project);
